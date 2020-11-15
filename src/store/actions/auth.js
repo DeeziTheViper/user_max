cf3f4a35f3d6f26a5f476f9476febc8cd915818e
Epic david <toonerdrill@gmail.com>
2020-11-15 03:43:31 +0100

    auth for login and signup

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -39,20 +39,54 @@ export const checkAuthTimeout = expirationTime => {
   };
 };
 
-export const authLogin = () => {
+export const authLogin = (email, password) => {
   return dispatch => {
     dispatch(authStart());
-
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user) {
-      dispatch(authSuccess(user));
-      dispatch(checkAuthTimeout(3600));
+    const user = {
+      email, password
     }
-    else {
-      dispatch(authFail());
-    }
+    axios
+      .post("/rest-auth/login/", user)
+      .then(res => {
+        if (res.data.key) {
+          const user = {
+            token: res.data.key,
+            username: res.data.user_detail.username,
+            expirationDate: new Date(new Date().getTime() + 3600 * 1000),
+            userId: res.data.user,
+            email: res.data.user_detail.email,
+            btc_wallet: res.data.user_detail.btc_wallet,
+            balance: res.data.user_detail.balance,
+            compounding: res.data.user_detail.compounding,
+            withdrawn: res.data.user_detail.withdrawn,
+            hash: res.data.user_detail.hash,
+            last_login: res.data.user_detail.last_login,
+            activeP: res.data.user_detail.activeP,
+            activeA: res.data.user_detail.activeA,
+            refLink: res.data.user_detail.Link,
+            first_name: res.data.user_detail.first_name,
+            last_name: res.data.user_detail.last_name,
+            country: res.data.user_detail.country,
+            address: res.data.user_detail.address,
+            zip_code: res.data.user_detail.zip_code,
+            city: res.data.user_detail.city,
+          }
+          localStorage.setItem("user", JSON.stringify(user));
+          dispatch(authSuccess(user));
+          dispatch(checkAuthTimeout(3600));
+        }
+        if (res.data.error) {
+          const detail = {
+            detail: res.data.error
+          }
+          dispatch(authFail(detail));
+        }
 
-  }
+      })
+      .catch(fail => {
+        dispatch(authFail({ detail: "Unable to Log you in" }));
+      });
+  };
 };
 
 
@@ -61,12 +95,11 @@ export const authSignup = (username, email, password1, password2, btc_wallet) =>
   return dispatch => {
     dispatch(authStart());
     const user = {
-      username, email, password1, password2, btc_wallet,
+      username, email, password1, password2, btc_wallet
     };
     axios
       .post("/rest-auth/registration/", user)
       .then(res => {
-
         if (res.data.detail) {
           const user = {
             detail: res.data.detail,
@@ -81,14 +114,14 @@ export const authSignup = (username, email, password1, password2, btc_wallet) =>
         }
 
         if (res.data.error) {
-          const detail = {
-            detail: res.data.error
+          const sev = {
+            sev: res.data.error
           }
-          dispatch(authFail(detail));
+          dispatch(authFailS(sev));
         }
       })
       .catch(fail => {
-        console.log(fail);
+        dispatch(authFailS({ detail: "Unable to proceed further" }));
       });
   };
 };
