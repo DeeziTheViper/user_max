05c33a5fa156f516bab808ee185b72b0652ae072
Epic david <toonerdrill@gmail.com>
2020-11-17 02:36:31 +0100

    extra reducer for authfail

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -75,6 +75,14 @@ const authLogout = (state, action) => {
   });
 };
 
+const authFailS = (state, action) => {
+  return updateObject(state, {
+    sev: action.error,
+    loading: false,
+    detailS: action.sev
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -83,6 +91,8 @@ const reducer = (state = initialState, action) => {
       return authSuccess(state, action);
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+    case actionTypes.AUTH_FAILS:
+      return authFailS(state, action);
     case actionTypes.AUTH_LOGOUT:
       return authLogout(state, action);
     default:
